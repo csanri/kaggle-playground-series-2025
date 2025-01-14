0f08ac2ddf0a6035b2721e906b2ae12a3a444e40
proxyvix <richard.csanadi@gmail.com>
2025-01-14 22:15:31 +0100

    s05e1 using gdp to look for correlation

--- forecasting-sticker-sales/forecasting-sticker-sales.py
+++ forecasting-sticker-sales/forecasting-sticker-sales.py
@@ -37,7 +37,7 @@ colors = sns.color_palette("tab10")
 fig, axes = plt.subplots(
     nrows=len(cat_cols),
     ncols=1,
-    figsize=(10, 10 * len(cat_cols))
+    figsize=(10, 5 * len(cat_cols))
 )
 
 for i, col in enumerate(cat_cols):
@@ -102,7 +102,7 @@ nrows = (len(countries) + ncols - 1) // ncols
 fig, axes = plt.subplots(
     nrows=nrows,
     ncols=ncols,
-    figsize=(20, 10 * nrows)
+    figsize=(5 * ncols, 5 * nrows)
 )
 
 axes = axes.flatten()
@@ -113,13 +113,198 @@ for i, country in enumerate(countries):
         x="date",
         y=target,
         ax=axes[i],
-        color=colors[i]
+        color=colors[i],
+        errorbar=None
     )
     print(country)
 
 plt.tight_layout()
 plt.savefig("images/country_lineplot.jpeg", dpi=300)
 plt.show()
+
+products = train_df["product"].unique()
+
+ncols = 2
+nrows = (len(products) + ncols - 1) // ncols
+
+fig, axes = plt.subplots(
+    nrows=nrows,
+    ncols=ncols,
+    figsize=(5 * ncols, 5 * nrows)
+)
+
+axes = axes.flatten()
+
+for i, product in enumerate(products):
+    sns.lineplot(
+        train_df.loc[train_df["product"] == product],
+        x="date",
+        y=target,
+        ax=axes[i],
+        color=colors[i],
+        errorbar=None
+    )
+    print(product)
+
+plt.tight_layout()
+plt.savefig("images/product_lineplot.jpeg", dpi=300)
+plt.show()
+
+stores = train_df["store"].unique()
+
+ncols = 3
+nrows = (len(stores) + ncols - 1) // ncols
+
+fig, axes = plt.subplots(
+    nrows=nrows,
+    ncols=ncols,
+    figsize=(5 * ncols, 5 * nrows)
+)
+
+axes = axes.flatten()
+
+for i, store in enumerate(stores):
+    sns.lineplot(
+        train_df.loc[train_df["store"] == store],
+        x="date",
+        y=target,
+        ax=axes[i],
+        color=colors[i],
+        errorbar=None
+    )
+    print(store)
+
+plt.tight_layout()
+plt.savefig("images/store_lineplot.jpeg", dpi=300)
+plt.show()
+
+country_weights = train_df.groupby("country")[
+    target
+].sum()/train_df[
+    target
+].sum()
+
+country_weights = country_weights.reset_index()
+
+plt.figure(figsize=(20, 16))
+
+sns.barplot(
+    country_weights,
+    x="country",
+    y=target
+)
+
+plt.tight_layout()
+plt.savefig("images/country_wights.jpeg", dpi=300)
+plt.show()
+
+weight_over_time = train_df.groupby(["date", "country"])[
+    target
+].sum()/train_df.groupby("date")[
+    target
+].sum()
+
+weight_over_time = weight_over_time.reset_index()
+
+plt.figure(figsize=(20, 16))
+
+sns.lineplot(
+    weight_over_time,
+    x="date",
+    y=target,
+    hue="country"
+)
+
+plt.tight_layout()
+plt.savefig("images/country_wights_over_time.jpeg", dpi=300)
+plt.show()
+
+"""
+The weights change over time,
+probably correlating with GPD
+"""
+
+gdp = pd.read_csv("API_NY.GDP.PCAP.CD_DS2_en_csv_v2_76.csv", skiprows=4)
+gdp = gdp[gdp["Country Name"].isin(countries)]
+
+min_date = min(train_df["date"]).year
+max_date = max(train_df["date"]).year
+
+years = [str(i) for i in range(min_date, max_date + 1)]
+
+gdp = gdp[["Country Name"] + years]
+
+for year in years:
+    gdp[year] = gdp[year]/gdp[year].sum()
+
+gdp = gdp.melt(
+    id_vars=["Country Name"],
+    var_name="Year",
+    value_name="GDP per Capita"
+)
+
+weight_over_time["date"] = weight_over_time["date"].dt.year
+
+gdp["Year"] = pd.to_datetime(
+    gdp["Year"],
+    format="ISO8601"
+).dt.year
+
+merged_df = pd.merge(weight_over_time, gdp,
+                     left_on=["date", "country"],
+                     right_on=["Year", "Country Name"],
+                     how="left"
+                     )
+
+plt.figure(figsize=(20, 16))
+
+sns.lineplot(
+    merged_df,
+    x="date",
+    y=target,
+    hue="country",
+)
+
+sns.lineplot(
+    merged_df,
+    x="Year",
+    y="GDP per Capita",
+    hue="country",
+    legend=False,
+    palette=["black"]*len(countries)
+)
+
+plt.tight_layout()
+plt.savefig("images/country_weights_and_gdp_over_time.jpeg", dpi=300)
+plt.show()
+
+"""
+for country in countries:
+    # Get rows with missing values for the target in the current country
+    na_values = train_df[(train_df[target].isna()) & (train_df["country"] == country)]
+
+    for index, row in na_values.iterrows():
+        year = row["date"].year
+
+        # Find matching GDP for the current country and year
+        matching_gdp = merged_df[(merged_df["Country Name"] == country) & (merged_df["Year"] == year)]
+
+        if not matching_gdp.empty:
+            # Get the GDP per capita value for the current country and year
+            gdp_value = matching_gdp["GDP per Capita"].values[0]
+
+            # Get the GDP ratio between Norway and the current country for the same year
+            ratio = merged_df[merged_df["Country Name"] == "Norway"]["GDP per Capita"].values[0] / gdp_value
+
+            # Get the total sales for the country in the same time period
+            total_sales_on_date = train_df[train_df["date"] == row["date"]][target].sum()
+
+            # Impute the missing value by multiplying the GDP ratio with the total sales on the same date
+            train_df.loc[index, target] = gdp_value * total_sales_on_date * ratio
+"""
+
+print(f"Missing values remaining: {train_df[target].isna().sum()}")
+print(train_df[target].value_counts())
 
 
 def date(df):
@@ -142,12 +327,10 @@ def date(df):
     df['Day_sin'] = np.sin(2 * np.pi * df['Day'] / 31)
     df['Day_cos'] = np.cos(2 * np.pi * df['Day'] / 31)
 
-    # df['Group'] = (df['Year']-2020)*48 + df['Month']*4 + df['Day']//7
-
     return df
 
 
-# train_df = date(train_df)
+train_df = date(train_df)
 
 cat_cols = train_df.select_dtypes(include="object").columns.tolist()
 cat_cols.remove("date")
