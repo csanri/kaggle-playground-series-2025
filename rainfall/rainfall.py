0bd6d88436dfb6e6d4db683e63e0abcf19eecc9a
proxyvix <richard.csanadi@gmail.com>
2025-03-17 13:14:16 +0100

    s05e3 corrected some logic

--- rainfall/rainfall.py
+++ rainfall/rainfall.py
@@ -8,11 +8,14 @@ from sklearn.metrics import roc_auc_score, classification_report
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 
 from datetime import datetime
 
 # Setting this so jupyter shows every column
 pd.set_option('display.max_columns', None)
+
+colors = sns.color_palette("tab10")
 
 # Reading data
 train_df = pd.read_csv("train.csv")
@@ -24,14 +27,13 @@ test_df = test_df.drop("id", axis=1)
 
 target = "rainfall"
 random_state = 42
-treshold = 0.5
 
-#################
-# DATA ANALYSIS #
-#################
+# ============================
+# DATA ANALYSIS
+# ============================
 print(train_df.info())
 print(50 * "=")
-print(train_df.describe().transpose())
+print(train_df.describe().T)
 print(50 * "=")
 print(train_df.isna().sum())
 print(test_df.isna().sum())
@@ -49,13 +51,12 @@ for col in train_df.columns:
     print(train_df[col].value_counts())
     print(50 * "-")
 
-cat_cols = train_df.select_dtypes(include="object").columns.tolist()
+# There are no categorical columns
 num_cols = train_df.select_dtypes(exclude="object").columns.tolist()
 
 num_cols.remove(target)
 
-colors = sns.color_palette("tab10")
-
+# Histograms of features
 # fig, axes = plt.subplots(
 #     nrows=3,
 #     ncols=4,
@@ -77,6 +78,7 @@ colors = sns.color_palette("tab10")
 # plt.savefig("images/num_cols.jpeg", dpi=300)
 # plt.show()
 
+# Correlation matrix masked to see only the lower triangle
 # corr_matrix = train_df.corr()
 
 # mask = np.triu(np.ones_like(corr_matrix, dtype=bool))
@@ -93,6 +95,7 @@ colors = sns.color_palette("tab10")
 # plt.savefig("images/corr_matrix.jpeg", dpi=300)
 # plt.show()
 
+# Distribution of the target
 # plt.figure(figsize=(20, 12))
 # sns.countplot(
 #     data=train_df,
@@ -103,6 +106,7 @@ colors = sns.color_palette("tab10")
 # plt.savefig("images/target.jpeg", dpi=300)
 # plt.show()
 
+# Boxplots of numerical features
 # fig, axes = plt.subplots(
 #     nrows=6,
 #     ncols=2,
@@ -123,6 +127,7 @@ colors = sns.color_palette("tab10")
 # plt.savefig("images/boxplots.jpeg", dpi=300)
 # plt.show()
 
+# Using KDE plots to see wheter a feature is predictive or not
 # fig, axes = plt.subplots(
 #     nrows=6,
 #     ncols=2,
@@ -144,6 +149,7 @@ colors = sns.color_palette("tab10")
 # plt.savefig("images/kde_plots.jpeg", dpi=300)
 # plt.show()
 
+# The day data has errors
 # plt.figure(figsize=(20, 16))
 # plt.plot(train_df["id"], train_df["day"])
 # plt.savefig("images/day_mismatch.jpeg", dpi=300)
@@ -157,14 +163,15 @@ train_df["day"] = expected_pattern[:len(train_df)]
 # plt.savefig("images/day_mismatch_corrected.jpeg", dpi=300)
 # plt.show()
 
-##################
-# PREPARING DATA #
-##################
+# ============================
+# PREPARING DATA
+# ============================
 
 print(train_df.skew())
 
 
 def add_features(df) -> pd.DataFrame:
+    # Months from days
     days_in_months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
     cum_days = [sum(days_in_months[:i+1]) for i in range(len(days_in_months))]
@@ -179,13 +186,16 @@ def add_features(df) -> pd.DataFrame:
         right=True
     )
 
+    # Day sin/cos for periodic features
     df["day_sin"] = np.sin(2 * np.pi * df["day"] / 365)
     df["day_cos"] = np.cos(2 * np.pi * df["day"] / 365)
 
+    # Last day's values for notable features
     df["cloud_lag1"] = df["cloud"].shift(-1).fillna(0)
     df["temp_lag1"] = df["temparature"].shift(-1).fillna(0)
     df["sunshine_lag1"] = df["sunshine"].shift(-1).fillna(0)
 
+    # Rolling mean for notable features
     df["cloud_roll_14"] = df["cloud"].rolling(
         window=14,
         min_periods=1
@@ -215,6 +225,7 @@ def add_features(df) -> pd.DataFrame:
 
     df["temp_range"] = df["maxtemp"] - df["mintemp"]
 
+    # 'Cloud' feature is skewed
     df["cloud_log"] = np.log1p(df["cloud"])
 
     return df
@@ -223,13 +234,15 @@ def add_features(df) -> pd.DataFrame:
 train = add_features(train_df)
 test = add_features(test_df)
 
+# Test data has an inf value for some reason
 test["sunshine_cloud"].replace(np.inf, 0, inplace=True)
 
-stats = ["mean", "median"]
+stats = ["mean", "median", "min", "max", "std"]
 
 group = "day"
 cols = ["cloud", "humidity", "sunshine"]
 
+# Aggregate statistical features on daily basis
 for col in cols:
     train_rain_stats = train.groupby(group)[col].agg(stats).reset_index()
     train_rain_stats.columns = [group] + [f"{group}_{col}_{stat}" for stat in stats]
@@ -238,15 +251,19 @@ for col in cols:
     test = test.merge(train_rain_stats, on=group, how="left")
 
 
-def onehot(df) -> pd.DataFrame:
+def onehot(df, column) -> pd.DataFrame:
     df = pd.get_dummies(
         df,
         prefix_sep="_",
-        columns=["month"],
+        columns=[column],
         dtype=np.int32,
     )
     return df
 
+
+# Onehot encoding month features
+train = onehot(train, "month")
+test = onehot(test, "month")
 
 # plt.figure(figsize=(12, 12))
 # sns.heatmap(
@@ -260,25 +277,6 @@ def onehot(df) -> pd.DataFrame:
 num_cols = train.select_dtypes(exclude="object").columns.tolist()
 num_cols.remove("rainfall")
 
-for df in [train, test]:
-    df = onehot(df)
-
-selected_features = train.columns.tolist()
-
-corr_matrix = train.corr().abs()
-
-upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
-high_correlation = [column for column in upper.columns if any(upper[column] > 0.85)]
-
-final_features = [f for f in selected_features if f not in high_correlation]
-
-train = train[final_features]
-final_features.remove(target)
-test = test[final_features]
-
-num_cols = train.select_dtypes(exclude="object").columns.tolist()
-num_cols.remove("rainfall")
-
 X_train, y_train = train.drop(target, axis=1), train[target]
 
 mms = MinMaxScaler()
@@ -286,9 +284,35 @@ mms = MinMaxScaler()
 X_train[num_cols] = mms.fit_transform(X_train[num_cols], y_train)
 test[num_cols] = mms.transform(test[num_cols])
 
-########################
-# TRAINING AND TESTING #
-########################
+# Using RFC to determine which features are important
+rf = RandomForestClassifier(
+    n_estimators=1000,
+    random_state=random_state,
+    class_weight="balanced"
+)
+rf.fit(X_train, y_train)
+
+important_features = np.argsort(rf.feature_importances_)[::-1][:20]
+
+selected_features = X_train.columns[important_features].tolist()
+
+corr_matrix = train[selected_features].corr().abs()
+
+# Dropping columns which have high correlation to each other
+# to reduce redundancy
+upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
+high_correlation = [column for column in upper.columns if any(upper[column] > 0.85)]
+
+final_features = [f for f in selected_features if f not in high_correlation]
+
+print(final_features)
+
+X_train = X_train[final_features]
+test = test[final_features]
+
+# ============================
+# TRAINING AND TESTING
+# ============================
 
 logreg_model = LogisticRegression(
     max_iter=1000,
@@ -305,13 +329,15 @@ svc_model = SVC(
     random_state=random_state
 )
 
-tsvc = TimeSeriesSplit(n_splits=5)
+tsvc = TimeSeriesSplit(n_splits=7)
 
 roc_auc_scores_logreg = []
 roc_auc_scores_svc = []
+roc_auc_scores_mean = []
 
 fold = 1
 
+# Time-Series split CV
 for train_idx, val_idx in tsvc.split(X_train):
     X_train_fold, X_val_fold = X_train.iloc[train_idx], X_train.iloc[val_idx]
     y_train_fold, y_val_fold = y_train.iloc[train_idx], y_train.iloc[val_idx]
@@ -319,44 +345,56 @@ for train_idx, val_idx in tsvc.split(X_train):
     logreg_model.fit(X_train_fold, y_train_fold)
     svc_model.fit(X_train_fold, y_train_fold)
 
-    pred_val_logreg = logreg_model.predict_proba(X_val_fold)[:, 1]
-    pred_val_logreg = (pred_val_logreg >= treshold).astype("int")
+    proba_val_logreg = logreg_model.predict_proba(X_val_fold)[:, 1]
+    pred_val_logreg = (proba_val_logreg >= 0.5).astype("int")
 
-    pred_val_svc = svc_model.predict_proba(X_val_fold)[:, 1]
-    pred_val_svc = (pred_val_svc >= treshold).astype("int")
+    proba_val_svc = svc_model.predict_proba(X_val_fold)[:, 1]
+    pred_val_svc = (proba_val_svc >= 0.5).astype("int")
 
-    roc_auc_logreg = roc_auc_score(y_true=y_val_fold, y_score=pred_val_logreg)
+    proba_val_mean = proba_val_logreg * 0.5 + proba_val_svc * 0.5
+    pred_val_mean = (proba_val_mean >= 0.5).astype("int")
+
+    roc_auc_logreg = roc_auc_score(y_true=y_val_fold, y_score=proba_val_logreg)
     roc_auc_scores_logreg.append(roc_auc_logreg)
 
-    roc_auc_svc = roc_auc_score(y_true=y_val_fold, y_score=pred_val_svc)
-    roc_auc_scores_logreg.append(roc_auc_svc)
+    roc_auc_svc = roc_auc_score(y_true=y_val_fold, y_score=proba_val_svc)
+    roc_auc_scores_svc.append(roc_auc_svc)
 
-    print(f"Fold {fold} - ROC-AUC Logreg: {roc_auc_logreg:.4f}")
-    print(classification_report(y_val_fold, pred_val_logreg))
-    print("=" * 60)
+    roc_auc_mean = roc_auc_score(y_true=y_val_fold, y_score=proba_val_mean)
+    roc_auc_scores_mean.append(roc_auc_mean)
 
-    print(f"Fold {fold} - ROC-AUC SVC: {roc_auc_svc:.4f}")
-    print(classification_report(y_val_fold, pred_val_svc))
-    print("=" * 60)
+    print("%-26s" % (f"Fold {fold}"))
+    print("-" * 26)
+    print("%-15s | %.4f |" % ("ROC-AUC LogReg:", roc_auc_logreg))
+    print("-" * 26)
+    print("%-15s | %.4f |" % ("ROC-AUC SVC:", roc_auc_svc))
+    print("-" * 26)
+    print("%-15s | %.4f |" % ("ROC-AUC Mean:", roc_auc_mean))
+    print("=" * 26)
 
     fold += 1
 
 
-print(f"Corss-Val score: {np.mean(roc_auc_scores_logreg):.4f}")
-print(f"Corss-Val score: {np.mean(roc_auc_scores_svc):.4f}")
+print(f"Corss-Val score LogReg: {np.mean(roc_auc_scores_logreg):.4f}")
+print(f"Corss-Val score SVC: {np.mean(roc_auc_scores_svc):.4f}")
+print(f"Corss-Val score Mean: {np.mean(roc_auc_scores_mean):.4f}")
 
-# pred_test = logreg_model.predict_proba(test)[:, 1]
-# pred_test = (pred_test >= treshold).astype("int")
+pred_test_logreg = logreg_model.predict_proba(test)[:, 1]
+pred_test_svc = svc_model.predict_proba(test)[:, 1]
 
-# submission = pd.DataFrame({
-#     "id": idx,
-#     target: pred_test
-# })
+pred_test = pred_test_logreg * 0.5 + pred_test_svc * 0.5
 
-# submission.to_csv(
-#     f"submissions/submission_{
-#         datetime.now().strftime("%Y%m%d_%H%M%S")
-#     }.csv",
-#     index=False
-# )
+submission = pd.DataFrame({
+    "id": idx,
+    target: pred_test
+})
+
+print(submission.head())
+
+submission.to_csv(
+    f"submissions/submission_{
+        datetime.now().strftime("%Y%m%d_%H%M%S")
+    }.csv",
+    index=False
+)
 
