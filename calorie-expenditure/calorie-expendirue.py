063218c7bd24b3c774334260fd1e63190bd6e2d9
proxyvix <richard.csanadi@gmail.com>
2025-05-29 18:42:07 +0200

    s5e5 experimenting with models

--- calorie-expenditure/calorie-expendirue.py
+++ calorie-expenditure/calorie-expendirue.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LinearRegression
 
 from xgboost import XGBRegressor
+from catboost import CatBoostRegressor
 
 # Setting this so jupyter shows every column
 pd.set_option('display.max_columns', None)
@@ -183,16 +184,16 @@ train_df["Age_Binned"] = pd.cut(
     right=False
 )
 
-calories_by_age = train_df.groupby(
-    ["Age_Binned"]
-)["Duration"].mean().reset_index()
+# calories_by_age = train_df.groupby(
+#     ["Age_Binned"]
+# )["Duration"].mean().reset_index()
 
-train_df = pd.merge(
-    train_df, calories_by_age,
-    on="Age_Binned",
-    how="left",
-    suffixes=("", "_Age_avg")
-)
+# train_df = pd.merge(
+#     train_df, calories_by_age,
+#     on="Age_Binned",
+#     how="left",
+#     suffixes=("", "_Age_avg")
+# )
 
 train_df["Age_Binned"] = train_df["Age_Binned"].map({
     "20_30": 0,
@@ -202,6 +203,31 @@ train_df["Age_Binned"] = train_df["Age_Binned"].map({
     "60_70": 4,
     "70_80": 5
 })
+
+num_cols = train_df.select_dtypes(exclude="object").columns.to_list()
+corr_matrix = train_df[num_cols].corr()
+
+plt.figure(figsize=(20, 20))
+
+sns.heatmap(
+    corr_matrix,
+    annot=True,
+    fmt=".2f",
+    cmap="magma"
+)
+
+plt.tight_layout()
+plt.savefig("figures/corr_matrix.png", dpi=300)
+plt.show()
+# NOTE: Drop highly correlated feature
+
+# train_df = train_df.drop(
+#     [
+#         "Height",
+#         "Weight"
+#     ],
+#     axis=1
+# )
 
 train, eval = train_test_split(train_df, train_size=0.8)
 
@@ -227,15 +253,19 @@ y_pred = np.expm1(y_pred)
 
 print(root_mean_squared_log_error(y_true=y_eval, y_pred=y_pred))
 
+######################
+# FEATURE IMPORTANCE #
+######################
+
 model_XGB = XGBRegressor(
     max_depth=6,
-    learning_rate=0.01,
-    n_estimators=1000
+    learning_rate=0.1,
+    n_estimators=1000,
+    eval_metric="rmsle",
+    random_state=42
 )
 
 model_XGB.fit(X_train, y_train)
-y_pred = model_XGB.predict(X_eval)
-y_pred = np.expm1(y_pred)
 
 importance_score = model_XGB.feature_importances_
 features = train_df.columns.to_list()
@@ -245,6 +275,17 @@ importance_df = pd.DataFrame({
     "Feature": features,
     "Importance": importance_score
 }).sort_values(by="Importance", ascending=False)
+
+# Selecting the top 8 important features
+top_features = importance_df.head(8)["Feature"].values
+
+X_train, y_train = train[top_features], train[target]
+X_eval, y_eval = eval[top_features], eval[target]
+
+scaler = MinMaxScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_eval = scaler.transform(X_eval)
 
 plt.figure(figsize=(20, 16))
 
@@ -258,37 +299,59 @@ sns.barplot(
 plt.tight_layout()
 plt.show()
 
-print(importance_score)
-print(root_mean_squared_log_error(y_true=y_eval, y_pred=y_pred))
+######
+
+model_XGB = XGBRegressor(
+    max_depth=6,
+    learning_rate=0.1,
+    n_estimators=10000,
+    eval_metric="rmsle",
+    random_state=42
+)
+
+model_CAT = CatBoostRegressor(
+    depth=6,
+    learning_rate=0.1,
+    iterations=10000,
+    loss_function="RMSE",
+    early_stopping_rounds=100,
+    random_state=42
+)
+
+model_XGB.fit(X_train, y_train)
+y_pred_XGB = model_XGB.predict(X_eval)
+y_pred_XGB = np.expm1(y_pred_XGB)
+
+model_CAT.fit(X_train, y_train)
+y_pred_CAT = model_CAT.predict(X_eval)
+y_pred_CAT = np.expm1(y_pred_CAT)
+
+print(f"XGB: {root_mean_squared_log_error(y_true=y_eval, y_pred=y_pred_XGB)}")
+print(f"CAT: {root_mean_squared_log_error(y_true=y_eval, y_pred=y_pred_CAT)}")
 
 fig, axes = plt.subplots(
-    nrows=1,
+    nrows=2,
     ncols=2,
     figsize=(20, 20)
 )
 
-sns.regplot(
-    x=y_pred,
-    y=y_eval,
-    line_kws={"color": "r"},
-    ax=axes[0]
-)
+for i, pred in enumerate([y_pred_XGB, y_pred_CAT]):
+    sns.regplot(
+        x=pred,
+        y=y_eval,
+        line_kws={"color": "r"},
+        ax=axes[0, i]
+    )
 
-y_res = abs(y_eval - y_pred)
+for i, pred in enumerate([y_pred_XGB, y_pred_CAT]):
+    y_res = abs(y_eval - pred)
 
-sns.regplot(
-    x=y_pred,
-    y=y_res,
-    line_kws={"color": "r"},
-    ax=axes[1]
-)
-
-plt.tight_layout()
-plt.show()
-
-plt.figure(figsize=(20, 20))
-
-sns.histplot(y_res, kde=True)
+    sns.regplot(
+        x=pred,
+        y=y_res,
+        line_kws={"color": "r"},
+        ax=axes[1, i]
+    )
 
 plt.tight_layout()
 plt.show()
